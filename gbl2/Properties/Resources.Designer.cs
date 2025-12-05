4a5616d06a1c402eb94add16a11eb205d0023099
Lebashhh <157116272+lebashhh@users.noreply.github.com>
2025-12-06 01:35:38 +0300

    Update Resources.Designer.cs

--- gbl2/Properties/Resources.Designer.cs
+++ gbl2/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ghuschyan2rkis.Properties
+namespace gbl2.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace ghuschyan2rkis.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ghuschyan2rkis.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("gbl2.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
