530434f04fb4ab53db2bb8633ca2fc91c489396e
Lebashhh <157116272+lebashhh@users.noreply.github.com>
2025-12-06 01:42:42 +0300

    Update AssemblyInfo.cs

--- gbl2/Properties/AssemblyInfo.cs
+++ gbl2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("ghuschyan2rkis")]
+[assembly: AssemblyTitle("gbl2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ghuschyan2rkis")]
+[assembly: AssemblyProduct("gbl2")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
